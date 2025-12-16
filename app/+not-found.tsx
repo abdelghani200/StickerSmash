1f45d1e844889264612c18c45fc87c2392e35dea
jelouani abdelghani <jelouaniabdelghani@gmail.com>
2025-12-16 18:00:23 +0100

    feat: add NotFound and About screens; update layout for navigation

---
+++ app/+not-found.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack, Link } from 'expo-router'
+
+const NotFoundScreen = () => {
+    return (
+        <>
+            <Stack.Screen options={{ title: 'Oops! Not Found' }}/>
+            <View>
+                <Link href="/">
+                    Go back to Home screen!
+                </Link>
+            </View>
+        </>
+    )
+}
+
+export default NotFoundScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#25292e",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: "underline",
+        color: "#fff",
+    }
+
+})
